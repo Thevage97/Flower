5fcfa1cb200b1d59c7e75e344fb5f7ee8669f845
Thevage97 <63274694+Thevage97@users.noreply.github.com>
2024-09-23 13:48:38 -0600

    Update and rename loto.py to draw_turtle_art.py

---
+++ draw_turtle_art.py
@@ -0,0 +1,22 @@
+import turtle
+import colorsys
+
+def draw_turtle_art():
+    turtle.tracer(100)
+    h = 0.85
+    turtle.speed(25)
+    turtle.bgcolor("black")
+    
+    for i in range(190):
+        c = colorsys.hsv_to_rgb(h,1,1)
+        turtle.fillcolor(c)
+        h += 0.0015
+        turtle.begin_fill()
+        turtle.circle(190-i, 90)
+        turtle.lt(75)
+        turtle.lt(20)
+        turtle.circle(190-i, 90)
+        turtle.lt(18)
+        turtle.end_fill()
+    
+    turtle.done()
